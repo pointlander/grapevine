5825f3ddd00e870ca0cc2ddef96e2452e17d6260
Andrew Snodgrass <git@pointlander.info>
2021-02-02 20:32:13 -0700

    WIP proof of concept

--- go.mod
+++ go.mod
@@ -3,6 +3,8 @@ module github.com/pointlander/grapevine
 go 1.15
 
 require (
+	github.com/c-bata/go-prompt v0.2.5
 	github.com/metricube/upnp v0.0.0-20150330145037-e91dfdd38f23
 	github.com/nictuku/dht v0.0.0-20201226073453-fd1c1dd3d66a
+	golang.org/x/crypto v0.0.0-20201221181555-eec23a3978ad
 )
--- main.go
+++ main.go
@@ -6,20 +6,78 @@ package main
 
 import (
 	"crypto/sha1"
+	"encoding/binary"
 	"encoding/hex"
 	"flag"
 	"fmt"
+	"log"
+	"net"
 	"os"
 	"os/signal"
+	"strings"
+	"sync"
 	"syscall"
 	"time"
 
+	"github.com/c-bata/go-prompt"
 	"github.com/metricube/upnp"
 	"github.com/nictuku/dht"
+	"golang.org/x/crypto/scrypt"
 )
 
-// Port is the server port
-var Port = flag.Int("port", 1234, "port")
+// Peer is a node peer
+type Peer struct {
+	Address string
+}
+
+// Message is a message
+type Message struct {
+	Message string
+	Cost    uint64
+	Time    time.Time
+}
+
+var (
+	// Port is the server port
+	Port       = flag.Int("port", 1337, "port")
+	u          *upnp.UPNP
+	nodes      = make(map[string]Peer)
+	nodesMutex sync.Mutex
+	address    string
+	messages   = make([]Message, 0, 8)
+)
+
+func cost(hash []byte) (cost uint64) {
+	for i, value := range hash {
+		if i == 0 {
+			cost = uint64(value & 1)
+			value >>= 1
+			for j := 1; j < 8; j++ {
+				if value&1 != 0 {
+					return cost
+				}
+				cost <<= 1
+				value >>= 1
+			}
+		} else {
+			for j := 0; j < 8; j++ {
+				if value&1 != 0 {
+					return cost
+				}
+				cost <<= 1
+				value >>= 1
+			}
+		}
+	}
+	return cost
+}
+
+func completer(d prompt.Document) []prompt.Suggest {
+	s := []prompt.Suggest{
+		{Text: "exit", Description: "Exit the application"},
+	}
+	return prompt.FilterHasPrefix(s, d.GetWordBeforeCursor(), true)
+}
 
 func main() {
 	flag.Parse()
@@ -33,49 +91,122 @@ func main() {
 	}
 	fmt.Println(ih)
 
-	u, err := upnp.NewUPNP()
+	ip := upnp.GetLocalAddress()
+	if ip != nil {
+		address = fmt.Sprintf("%v:%d", ip, *Port)
+	}
+	u, err = upnp.NewUPNP()
+	if err != nil {
+		u = nil
+	}
+	if u != nil {
+		u.AddPortMapping(*Port, *Port, "UDP")
+		u.AddPortMapping(*Port+2, *Port+2, "UDP")
+		ip, err := u.ExternalIPAddress()
+		if err != nil {
+			panic(err)
+		}
+		address = fmt.Sprintf("%v:%d", ip, *Port)
+	}
+
+	config := dht.NewConfig()
+	config.Port = *Port
+	d, err := dht.New(config)
 	if err != nil {
 		panic(err)
 	}
-	u.AddPortMapping(*Port, *Port, "UDP")
-
-	d, err := dht.New(nil)
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "New DHT error: %v", err)
-		os.Exit(1)
-
-	}
 
 	if err = d.Start(); err != nil {
-		fmt.Fprintf(os.Stderr, "DHT start error: %v", err)
-		os.Exit(1)
+		panic(err)
 	}
 	go drainresults(d)
 
-	c := make(chan os.Signal)
+	c, done := make(chan os.Signal), make(chan bool)
 	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
 	go func() {
-		<-c
-		fmt.Println("\r- Ctrl+C pressed in Terminal")
-		u.DelPortMapping(*Port, "UDP")
+		select {
+		case <-c:
+		case <-done:
+		}
+		fmt.Println("\rCleaning up...")
+		if u != nil {
+			u.DelPortMapping(*Port, "UDP")
+			u.DelPortMapping(*Port+2, "UDP")
+		}
 		d.RemoveInfoHash(string(ih))
 		os.Exit(0)
 	}()
 
+	ticker, packets, commands := time.Tick(5*time.Second), make(chan []byte, 8), make(chan string, 8)
+	go func() {
+		connection, err := net.ListenPacket("udp", fmt.Sprintf(":%d", *Port+2))
+		if err != nil {
+			return
+		}
+		defer connection.Close()
+		for {
+			buffer := make([]byte, 1024+8*8)
+			_, _, err := connection.ReadFrom(buffer)
+			if err != nil {
+				log.Println(err)
+				return
+			}
+			packets <- buffer
+		}
+	}()
+	go func() {
+		for {
+			command := prompt.Input("> ", completer)
+			if command == "exit" {
+				done <- true
+				return
+			}
+			commands <- command
+		}
+	}()
 	for {
-		d.PeersRequestPort(string(ih), true, *Port)
-		time.Sleep(5 * time.Second)
+		select {
+		case <-ticker:
+			d.PeersRequestPort(string(ih), true, *Port+2)
+		case packet := <-packets:
+			text := packet[:1024]
+			proofs := packet[1024:]
+			total := uint64(0)
+			for i := 0; i < len(proofs); i += 8 {
+				hash, err := scrypt.Key(text, proofs[i:i+8], 32768, 8, 1, 32)
+				if err != nil {
+					log.Println(err)
+				}
+				total += cost(hash)
+			}
+			runes := make([]rune, 256)
+			for i := range runes {
+				runes[i] = rune(binary.LittleEndian.Uint32(text[i*4 : i*4+4]))
+			}
+			messages = append(messages, Message{
+				Message: string(runes),
+				Cost:    total,
+				Time:    time.Now(),
+			})
+		case command := <-commands:
+			parts := strings.Split(command, " ")
+			if parts[0] == "send" {
+
+			}
+		}
 	}
 }
 
 func drainresults(n *dht.DHT) {
-	fmt.Println("=========================== DHT")
-	fmt.Println("Note that there are many bad nodes that reply to anything you ask.")
-	fmt.Println("Peers found:")
 	for r := range n.PeersRequestResults {
 		for _, peers := range r {
 			for _, x := range peers {
-				fmt.Printf("%v\n", dht.DecodePeerAddress(x))
+				peer := dht.DecodePeerAddress(x)
+				nodesMutex.Lock()
+				nodes[peer] = Peer{
+					Address: peer,
+				}
+				nodesMutex.Unlock()
 			}
 		}
 	}
